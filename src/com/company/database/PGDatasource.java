a6e56ba27944d70975849d01f2481c9f23a05f53
Jan Marti <jan_marti@bluewin.ch>
2013-10-03 17:58:49 +0200

    import statement deleted

--- src/com/company/database/PGDatasource.java
+++ src/com/company/database/PGDatasource.java
@@ -7,7 +7,6 @@ import java.sql.*;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.text.SimpleDateFormat;
 
 /**
  * Created with IntelliJ IDEA.
